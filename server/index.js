c083b96237f1b1f14e11b875cf8fdcfa7ce87ca4
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-11-16 16:38:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,40 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 //components
-import Connection from './database/db.js';
-import Router from './routes/route.js';
-
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://dailybuglebackend.onrender.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
+app.use(cors());
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', Router);
-
+app.use("/", Router);
 
 const PORT = 8000;
 const username = process.env.DB_USERNAME;
@@ -42,4 +23,6 @@ const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
