e65b63cf9bf4676df06724ead7c193ee4e13aad5
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-11-20 21:49:17 +0530

    Update upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -8,7 +8,7 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;  
 
 const storage = new GridFsStorage({
-    url: `mongodb://${username}:${password}@ac-ppjwgqq-shard-00-00.2zv2izu.mongodb.net:27017,ac-ppjwgqq-shard-00-01.2zv2izu.mongodb.net:27017,ac-ppjwgqq-shard-00-02.2zv2izu.mongodb.net:27017/?ssl=true&replicaSet=atlas-zf8bd8-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    url: `mongodb://${username}:${password}@ac-tnw6pdn-shard-00-00.fpoo15i.mongodb.net:27017,ac-tnw6pdn-shard-00-01.fpoo15i.mongodb.net:27017,ac-tnw6pdn-shard-00-02.fpoo15i.mongodb.net:27017/?ssl=true&replicaSet=atlas-vt2sdv-shard-0&authSource=admin&retryWrites=true&w=majority`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
